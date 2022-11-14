00a3aadef9e5f6b804a33cd5252ad5d5b7b04ee1
Igor <103461835+Hell-Writer@users.noreply.github.com>
2022-11-14 19:20:06 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,4 @@
+ROOT = 'https://api.telegram.org/'
+BASE_CHAT_ID = '743501060'
+RAND_CAT_URL = 'https://api.thecatapi.com/v1/images/search'
+RAND_DOG_URL = 'https://api.thedogapi.com/v1/images/search'
